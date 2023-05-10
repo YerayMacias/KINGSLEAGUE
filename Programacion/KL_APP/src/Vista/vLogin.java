c9030d548aef34868fa62ca82f990d875361cc05
Iker Zambrano <ikerunai.zambrano@ikasle.egibide.org>
2023-05-10 13:58:20 +0200

    Revert "Revert "Final del dia""
    This reverts commit 5dac43656a7ae3d3336486fc875c43f6cb65f646.

--- Programacion/KL_APP/src/Vista/vLogin.java
+++ Programacion/KL_APP/src/Vista/vLogin.java
@@ -8,6 +8,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.FocusAdapter;
 import java.awt.event.FocusEvent;
+import java.util.regex.Pattern;
 
 public class vLogin {
     private JPanel pPrincipal;
@@ -55,7 +56,13 @@ public class vLogin {
     public void validar(String nombre, String contraseña){
         // validar datos
         try {
-            Main.validarUsuario(nombre, contraseña);
+            if (nombre.length() < 3)
+                throw new Exception();
+        } catch (Exception e){
+            System.out.println("Problemas con: " + e.getMessage());
+        }
+        try {
+            //Main.validarUsuario(nombre, contraseña);
         } catch (Exception e) {
             System.out.println("Problemas " + e.getMessage());;
         }
