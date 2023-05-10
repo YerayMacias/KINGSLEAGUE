4cc195386ad5545787c298b1606a2beeda4a7330
Yeray Macias <yeray.macias@ikasle.egibide.org>
2023-05-10 13:04:00 +0200

    Añadidas Vistas Clasificacion / Partidos

--- Programacion/KL_APP/src/Vista/vClasificacion.java
+++ Programacion/KL_APP/src/Vista/vClasificacion.java
@@ -2,20 +2,22 @@ package Vista;
 
 import javax.swing.*;
 
-public class vPrincipal {
-    private JPanel pPrincipal;
+public class vClasificacion {
     private JPanel pMenu;
-    private JPanel pDatos;
-    private JPanel pPartidos;
     private JLabel lNombre;
+    private JPanel pCombo;
+    private JPanel pClasificacion;
+    private JPanel pTitulo;
+    private JPanel pHeader;
+    private JPanel pPrincipal;
 
     public JPanel getpPrincipal() {
         return pPrincipal;
     }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("vPrincipal");
-        frame.setContentPane(new vPrincipal().getpPrincipal());
+        JFrame frame = new JFrame("vClasificacion");
+        frame.setContentPane(new vClasificacion().getpPrincipal());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
